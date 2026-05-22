5289199ce38357e5914e3bdc2bf1791f040d055b
Lucy Wambui <lucykangacha2@gmail.com>
2026-05-22 15:52:04 +0300

    Add files via upload

---
+++ patient_system.py
@@ -0,0 +1,10 @@
+print("=== Patient Registration System ===")
+
+name = input("Enter patient name: ")
+age = input("Enter patient age: ")
+status = input("Is patient new or existing? ")
+
+print("\n--- Patient Details ---")
+print("Patient Name:", name)
+print("Patient Age:", age)
+print("Patient Status:", status)
